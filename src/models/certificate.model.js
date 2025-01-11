7a28177ada6612e3523377fbd0f2452472665f02
divyashantkumar <divyashantkumar000@gmail.com>
2025-01-11 14:22:11 +0530

    Certificate Schema finished

--- src/models/certificate.model.js
+++ src/models/certificate.model.js
@@ -1,5 +1,11 @@
 /*
-    
+    id string pk
+    user_id string fk
+    name string
+    category string
+    issued_date Date
+    expiry_date Date
+    certificate_url string
 */
 
 import { Schema, model } from 'mongoose';
@@ -7,7 +13,41 @@ import { Schema, model } from 'mongoose';
 
 const certificateSchema = new Schema(
     {
-        
+        user_id: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            unique: true,
+            required: [true, 'User id is required']
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: [true, 'Certificate name is required'],
+        },
+        category: {
+            type: String,
+            trim: true,
+            // enum: {
+            //     values: ['deploma', 'graduation', 'postGraduation', 'doctorate', 'other'],
+            //     message: '{VALUE} is not a valid certificate category'
+            // },
+            required: [true, 'Certificate category is required'],
+        },
+        issued_date: {
+            type: Date,
+            trim: true,
+            required: [true, 'Certificate issued date is required'],
+        },
+        expiry_date: {
+            type: Date,
+            trim: true,
+            required: [true, 'Certificate expiry date is required'],
+        },
+        certificate_url: {
+            type: String,
+            trim: true,
+            required: [true, 'Certificate url is required'],
+        }
     },
     {
         timestamps: true,
