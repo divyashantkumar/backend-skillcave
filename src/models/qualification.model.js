f40b9dcd442570043983a97f056dc62294d54da1
divyashantkumar <divyashantkumar000@gmail.com>
2025-01-11 13:31:08 +0530

    Finished: Qualification Schema

--- src/models/qualification.model.js
+++ src/models/qualification.model.js
@@ -1,5 +1,14 @@
 /*
-    
+    id ObjectId pk
+    user_id ObjectId fk
+    type string // [deploma, graduation, postGraduation, doctorate, "other"]
+    duration string // [Degree, Certification, Diploma]
+    field_of_study string
+    completion_status string
+    start_date Date
+    end_date Date
+    percentage number
+    institute ObjectId fk
 */
 
 import { Schema, model } from 'mongoose';
@@ -7,7 +16,56 @@ import { Schema, model } from 'mongoose';
 
 const qualificationSchema = new Schema(
     {
-        
+        user_id: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            unique: true,
+            required: [true, 'User id is required']
+        },
+        type: {
+            type: String,
+            enum: {
+                values: ['deploma', 'graduation', 'postGraduation', 'doctorate', 'other'],
+                message: '{VALUE} is not a valid qualification type'
+            },
+            required: true
+        },
+        duration: {
+            type: Number,
+            required: false
+        },
+        field_of_study: {
+            type: String,
+            lowercase: true,
+            required: false
+        },
+        completion_status: {
+            type: String,
+            enum: {
+                values: ['completed', 'ongoing', 'dropped'],
+                message: '{VALUE} is not a valid completion status'
+            },
+            required: false
+        },
+        start_date: {
+            type: Date,
+            required: false
+        },
+        end_date: {
+            type: Date,
+            required: false
+        },
+        percentage: {
+            type: Number,
+            min: 0,
+            max: 100,
+            required: false,
+        },
+        institute: {
+            type: Schema.Types.ObjectId,
+            ref: 'Institute',
+            required: false
+        }
     },
     {
         timestamps: true,
@@ -19,6 +77,10 @@ const qualificationSchema = new Schema(
         }
     }
 );
+
+
+qualificationSchema.index({ user_id: 1 }, { unique: true });
+
 
 const Qualification = model('Qualification', qualificationSchema);
 export default Qualification;
