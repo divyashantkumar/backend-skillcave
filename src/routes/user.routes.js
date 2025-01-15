b02c7c40d7a7f9ad23f1903d8374573d987bead2
divyashantkumar <divyashantkumar000@gmail.com>
2025-01-15 17:19:43 +0530

    Added: routes in user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,29 @@
 import { Router } from "express";
+import { upload } from "../middlewares/multer.middleware.js";
+import { registerUser } from "../controllers/user.controllers.js";
 
 const router = Router();
 
-router.route('/register').post();
+router.route('/register').post(
+    upload.fields(
+        [
+            { name: 'avatar', maxCount: 1 },
+            { name: 'resume', maxCount: 1 }
+        ]
+    ),
+    registerUser
+);
+
+router.route('/send-email-otp').post();
+
+router.route('/verify-email-otp').post();
+
+router.route('/send-phone-otp').post();
+
+router.route('/verify-phone-otp').post();
+
+router.route('/login').post();
+
 
 export default router;
 
