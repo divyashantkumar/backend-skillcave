7651ab5400cba80dc7cd3c71ade9e7929c81adc7
divyashantkumar <divyashantkumar000@gmail.com>
2025-01-15 17:15:13 +0530

    Added: Function to upload files to some cloud based storage

---
+++ src/utils/fileUploadtoCloud.js
@@ -0,0 +1,22 @@
+
+
+export const uploadFileToCloud = async (file) => {
+    const filePath = file?.path;
+    const fileUrl = "";
+
+    return fileUrl;
+};
+
+/*
+    file: {
+        fieldname: 'resume',
+        originalname: 'd733a854-2bac-4268-81ad-bb0df5cc9fad.pdf',
+        encoding: '7bit',
+        mimetype: 'application/pdf',
+        destination: 'C:\\Users\\guess\\Desktop\\SkillCave\\backend-skillcave\\public\\uploads',
+        filename: 'd733a854-2bac-4268-81ad-bb0df5cc9fad.pdf',
+        path: 'C:\\Users\\guess\\Desktop\\SkillCave\\backend-skillcave\\public\\uploads\\d733a854-2bac-4268-81ad-bb0df5cc9fad.pdf',     
+        size: 66823
+    }
+*/
+
