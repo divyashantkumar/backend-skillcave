0d07ad827e715f16431657fdb08e1747230a1bbc
divyashantkumar <divyashantkumar000@gmail.com>
2025-01-15 17:19:12 +0530

    Finished: Register controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,28 +3,80 @@ import CustomError from '../errors/custom.error.js';
 import User from '../models/user.model.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
 import { generateTokens, cookieOptions } from '../utils/token.js';
+import Qualification from '../models/qualification.model.js';
+import { uploadFileToCloud } from '../utils/fileUploadtoCloud.js';
 
 
 export const registerUser = asyncHandler(async (req, res) => {
-    const { name, email, phone, password, role = "student" } = req.body;
+    const {
+        first_name,
+        last_name,
+        email,
+        phone,
+        qualification,
+        bio,
+        role = "user"
+    } = req.body;
 
     // TODO: Validate the request body parameters GLobally
 
-    const userExists = await User.findOne({ email: email.toLowerCase() }).exec();
+    const user = await User.findOne({ email: email.toLowerCase() }).exec();
 
-    if (userExists) {
-        throw new CustomError(400, "User already exists with this email");
+    if (user?.email) {
+        return res
+            .status(400)
+            .json(
+                new ApiResponse(400, "Email already in use")
+            );
     }
 
-    const user = await User.create({
-        name,
+    if (user?.phone) {
+        return res
+            .status(400)
+            .json(
+                new ApiResponse(400, "Phone number already exists")
+            );
+    }
+
+    // upload resume FIle to cloud
+    let avatarUrl = "";
+    if (req.files?.avatar) {
+        avatarUrl = await uploadFileToCloud(req.files.avatar[0]);
+    }
+
+    let resumeUrl = "";
+    if (req.files?.resume) {
+        resumeUrl = await uploadFileToCloud(req.files.resume[0]);
+    }
+
+    const newUser = await User.create({
+        first_name,
+        last_name,
         email: email.toLowerCase(),
         phone,
-        password,
         role,
+        bio,
+        avatar: avatarUrl,
+        resume: resumeUrl
     });
 
-    await user.updateLastActiveTime();
+    if (!newUser) {
+        return res
+            .status(400)
+            .json(
+                new ApiResponse(400, "User Registration Failed")
+            );
+    }
+
+    const qualificationDoc = await Qualification.create({
+        user_id: newUser._id,
+        type: qualification.toLowerCase(),
+    })
+
+    newUser.qualifications.push(qualificationDoc._id);
+    await newUser.save();
+    await newUser.updateLastActiveTime();
+
 
     const { accessToken, refreshToken } = generateTokens(user);
 
@@ -37,7 +89,7 @@ export const registerUser = asyncHandler(async (req, res) => {
         })
         .json(
             new ApiResponse(200, "User created successfully", {
-                user,
+                newUser,
                 accessToken,
                 refreshToken,
             })
@@ -79,4 +131,7 @@ export const loginUser = asyncHandler(async (req, res) => {
             })
         )
 });
+
+
+
 
