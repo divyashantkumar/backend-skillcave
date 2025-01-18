5aed9534d6a96a80854f5d61d03444bc9728e30f
divyashantkumar <divyashantkumar000@gmail.com>
2025-01-18 17:47:25 +0530

    updated: Question Model

--- src/models/question.model.js
+++ src/models/question.model.js
@@ -11,9 +11,47 @@
 
 import { Schema, model } from 'mongoose';
 
+const optionSchema = new Schema(
+    {
+        option: {
+            type: String,
+            required: [true, 'Question option is required'],
+            trim: true,
+        },
+        option_type: {
+            type: String,
+            enum: {
+                values: ["text", "code", "image", "video"],
+                message: '{VALUE} is not a valid option\'s type'
+            },
+            default: "text"
+        },
+        option_score: {
+            type: Number,
+            min: 0,
+            default: 0
+        }
+    },
+    {
+        _id: false,
+        id: false
+    }
+)
+
 
 const questionSchema = new Schema(
     {
+        user_id: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            unique: true,
+            required: [true, 'User id is required']
+        },
+        question: {
+            type: String,
+            trim: true,
+            required: [true, 'Question is required'],
+        },
         category: {
             type: String,
             enum: {
@@ -23,46 +61,26 @@ const questionSchema = new Schema(
             trim: true,
             required: [true, 'Question category is required'],
         },
-        score: {
-            type: Number,
-            trim: true,
-            required: [true, 'Question score is required'],
-        },
+        options: [optionSchema],
         duration: {
-            type: Number,
-            trim: true,
-            required: [true, 'Question duration is required'],
-        },
-        question: {
-            type: String,
-            trim: true,
-            required: [true, 'Question is required'],
-        },
-        options: [
-            {
-                option: {
-                    type: String,
-                    trim: true,
+            length: {
+                type: Number,
+                trim: true,
+                required: [true, 'Question duration is required'],
+            },
+            unit: {
+                type: String,
+                enum: {
+                    values: ['seconds', 'minutes', 'hours'],
+                    message: '{VALUE} is not a valid question duration unit'
                 },
-                options_type: {
-                    type: String,
-                    enum: {
-                        values: ["text", "code", "image", "video"],
-                        message: '{VALUE} is not a valid option\'s type'
-                    },
-                    default: "text"
-                },
-                option_score : {
-                    type: Number,
-                    min: 0,
-                    default: 0
-                }
+                trim: true,
+                required: [true, 'Question duration unit is required'],
             }
-        ],
-        correct_answer: {
+        },
+        answer: {
             type: String,
             trim: true,
-            required: [false, 'Question correct answer is required'],
         }
     },
     {
