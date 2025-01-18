f4d648fd4e7cd2e12d193dd7ef08c160bf808e51
divyashantkumar <divyashantkumar000@gmail.com>
2025-01-18 17:48:45 +0530

    Added : post and delete route to question router

--- src/routes/question.routes.js
+++ src/routes/question.routes.js
@@ -1,12 +1,19 @@
 import { Router } from "express";
 import { isAuthenticated, restrictTo } from "../middlewares/auth.middleware.js";
+import { addQuestion, deleteQuestion } from "../controllers/question.controllers.js";
 
 const router = Router();
 
 router.route('/').post(
     isAuthenticated, 
     restrictTo(['admin', 'instructor']), 
-    (req, res) => { }
+    addQuestion
+);
+
+router.route('/').delete(
+    isAuthenticated, 
+    restrictTo(['admin', 'instructor']), 
+    deleteQuestion
 );
 
 
